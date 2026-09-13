711621f6e00544614bd7b51ba00a2b95efde75d9
viniciusvictor07 <srzeldris12@gmail.com>
2026-09-13 14:31:52 -0300

    refactor: improve main class flow

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,42 +1,15 @@
 package application;
 
-import entities.Game;
+import entities.LibraryManager;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        ArrayList<Game> gamesLibrary = new ArrayList<>();
-        String selectedName;
-        String selectedFranchise;
-        String selectedRanking;
-        String selectedPendingStatus;
+        Scanner sc = new Scanner(System.in);
+        LibraryManager libraryManager = new LibraryManager(sc);
 
-        System.out.println("Enter game name:");
-        selectedName = input.nextLine();
-
-        System.out.println("Enter game franchise:");
-        selectedFranchise = input.nextLine();
-
-        System.out.println("Enter game ranking:");
-        selectedRanking = input.nextLine();
-
-        System.out.println("Enter game pending status:");
-        selectedPendingStatus = input.nextLine();
-
-        Game pendingGame = new Game(selectedName, selectedFranchise, selectedRanking, selectedPendingStatus);
-        gamesLibrary.add(pendingGame);
-
-        System.out.println("Games list:");
-        for (Game game : gamesLibrary) {
-            System.out.printf("%s - %s - %s - %s\n",
-                    game.getName(),
-                    game.getFranchise(),
-                    game.getRanking(),
-                    game.getPendingStatus());
-        }
-        input.close();
+        libraryManager.addGameFlow();
+        sc.close();
     }
 }
