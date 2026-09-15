9042bd97d8531d01b1cc1676ae922c9459860765
viniciusvictor07 <srzeldris12@gmail.com>
2026-09-15 19:39:06 -0300

    refactor: improve main class flow

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,15 +1,20 @@
 package application;
 
+import ui.ConsoleUI;
+import entities.Game;
 import entities.LibraryManager;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        LibraryManager libraryManager = new LibraryManager(sc);
+        ArrayList<Game> gamesLibrary = new ArrayList<>();
+        LibraryManager libraryManager = new LibraryManager(sc, gamesLibrary);
+        ConsoleUI consoleUI = new ConsoleUI(sc, libraryManager);
+        consoleUI.process();
 
-        libraryManager.addGameFlow();
         sc.close();
     }
 }
